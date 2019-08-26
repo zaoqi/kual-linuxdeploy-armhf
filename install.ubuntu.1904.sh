54c89ab830366ee091eac2a0f90283b8a56ddeca
zaoqi <zaomir@outlook.com>
2019-08-27 00:11:17 +0800

    .

--- install.ubuntu.1904.sh
+++ install.ubuntu.1904.sh
@@ -4,7 +4,7 @@
 
 [ -f "$ROOTFS_IMG" ] && fail "rootfs exist.Please remove it and continue."
 
-mirror=http://cdimage.ubuntu.com/ubuntu-cdimage
+mirror=http://cdimage.ubuntu.com
 
 get_rootfs_tgz_url(){
     echo "$mirror/ubuntu-base/releases/19.04/release/ubuntu-base-19.04-base-armhf.tar.gz"
