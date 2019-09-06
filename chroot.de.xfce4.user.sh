2d89f028d3cf880e79d1a7f0aff7d779dfb71d2d
zaoqi <zaomir@outlook.com>
2019-09-06 19:23:04 +0800

    egrep -> grep

--- chroot.de.xfce4.user.sh
+++ chroot.de.xfce4.user.sh
@@ -4,7 +4,7 @@
 
 [ -f "$ROOTFS_LOCK" ] || mount_rootfs_all
 
-SIZE=$(xwininfo -root -display :0 | egrep "geometry" | cut -d " "  -f4)
+SIZE=$(xwininfo -root -display :0 | grep "geometry" | cut -d " "  -f4)
 
 sleep 3 # wait kterm
 do_chroot /bin/sh -c "su -l kindle sh -c 'DISPLAY=:0 Xephyr :1 -title L:D_N:application_ID:xephyr -ac -br -screen $SIZE -cc 4 -reset -terminate & sleep 3 && DISPLAY=:1 xfce4-session'"
